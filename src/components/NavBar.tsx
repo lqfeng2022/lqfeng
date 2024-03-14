30fe2e3734c1658455c782c0dc0c5def62d85ae5
Supercoder8888 <simonlqf@hotmail.com>
2024-03-14 15:50:36 +0800

    Add few more items in Navigation Bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,8 +3,14 @@ import { HStack, Text } from "@chakra-ui/react";
 const NavBar = () => {
   return (
     <HStack justifyContent="space-between" padding={5}>
-      <Text>LI QIUFENG</Text>
-      <Text>NavBar</Text>
+      <Text fontSize="2xl" marginY="8px">LI QIUFENG</Text>
+      <HStack>
+        <Text marginY="8px">WORK</Text>
+        <Text marginY="8px" paddingX="16px">
+          PROJECT
+        </Text>
+        <Text marginY="8px">ME</Text>
+      </HStack>
     </HStack>
   );
 };
