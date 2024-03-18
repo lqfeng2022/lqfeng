2050d7d3764f48cd1a448c86b33c6712e24ddf06
Supercoder8888 <simonlqf@hotmail.com>
2024-03-18 13:43:45 +0800

    Add links to the items of 'NavBar' and do some refactoring..

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,15 @@
 import { HStack, Heading, Icon } from "@chakra-ui/react";
 import { HiLanguage } from "react-icons/hi2";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const NavBar = () => {
+  const location = useLocation();
+  const links = [
+    { id: 0, name: "W O R K", url: "/" },
+    { id: 1, name: "P R O D U C T", url: "/product" },
+    { id: 2, name: "M E", url: "/about-me" },
+  ];
+
   return (
     <HStack justifyContent="space-between" padding="10px">
       <Link to="/">
@@ -11,21 +18,20 @@ const NavBar = () => {
         </Heading>
       </Link>
       <HStack spacing="25px">
-        <Link className="border-bottom border-dark" to="/">
-          <Heading fontSize="xs" marginY="5px" className="fw-lighter">
-            W O R K
-          </Heading>
-        </Link>
-        <Link to="product">
-          <Heading fontSize="xs" marginY="5px" className="fw-lighter">
-            P R O D U C T
-          </Heading>
-        </Link>
-        <Link to="about-me">
-          <Heading fontSize="xs" marginY="5px" className="fw-lighter">
-            M E
-          </Heading>
-        </Link>
+        {links.map((link) => (
+          <Link
+            className={
+              location.pathname === `${link.url}`
+                ? "border-bottom border-dark"
+                : ""
+            }
+            to={link.url}
+          >
+            <Heading fontSize="xs" marginY="5px" className="fw-lighter">
+              {link.name}
+            </Heading>
+          </Link>
+        ))}
         <Icon as={HiLanguage} boxSize="20px" color="gray.600" />
       </HStack>
     </HStack>
