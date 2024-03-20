489e327de5c8034a402e149f6260955e28b140f1
Supercoder8888 <simonlqf@hotmail.com>
2024-03-20 13:08:20 +0800

    Create the self-introduction content in 'Me' page..

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -10,31 +10,29 @@ const NavBar = () => {
   ];
 
   return (
-    <>
-      <HStack justifyContent="space-between" px="30px" py="20px">
-        <Link to="/">
-          <Heading fontSize="2xl" my="8px" className="fw-lighter">
-            LI QIUFENG
-          </Heading>
-        </Link>
-        <HStack spacing="25px">
-          {links.map((link) => (
-            <Link
-              key={link.id}
-              to={link.url}
-              className={
-                l.pathname === `${link.url}` ? "border-bottom border-dark" : ""
-              }
-            >
-              <Heading fontSize="sm" my="3px" className="fw-lighter">
-                {link.name}
-              </Heading>
-            </Link>
-          ))}
-          <Icon as={HiLanguage} boxSize="20px" color="gray.600" />
-        </HStack>
+    <HStack justifyContent="space-between" px="30px" py="20px">
+      <Link to="/">
+        <Heading fontSize="2xl" my="8px" className="fw-lighter">
+          LI QIUFENG
+        </Heading>
+      </Link>
+      <HStack spacing="25px">
+        {links.map((link) => (
+          <Link
+            key={link.id}
+            to={link.url}
+            className={
+              l.pathname === `${link.url}` ? "border-bottom border-dark" : ""
+            }
+          >
+            <Heading fontSize="sm" my="3px" className="fw-lighter">
+              {link.name}
+            </Heading>
+          </Link>
+        ))}
+        <Icon as={HiLanguage} boxSize="20px" color="gray.600" />
       </HStack>
-    </>
+    </HStack>
   );
 };
 
--- src/pages/Me.tsx
+++ src/pages/Me.tsx
@@ -1,13 +1,41 @@
-import { Box, Text } from "@chakra-ui/react";
+import {
+  Box,
+  SimpleGrid,
+  Text,
+  Image,
+  Heading,
+  Show,
+  Center,
+} from "@chakra-ui/react";
+import photo from "../assets/lqfeng.jpeg";
 
-const Me = () => {
-  return (
-    <Box paddingX="30px" paddingY="25px">
-      <Text fontSize="xl" paddingBottom={5} style={{ letterSpacing: "1px" }}>
-        Me
-      </Text>
-    </Box>
-  );
-};
+const Me = () => (
+  <Center px="30px" py="25px">
+    <SimpleGrid columns={[1, null, 2]} maxW="1200px" spacing="40px">
+      <Center>
+        <Box maxW="708px">
+          <Image src={photo} />
+          <Show above="md">
+            <Box h="180px" />
+          </Show>
+        </Box>
+      </Center>
+      <Center>
+        <Box maxW="708px">
+          <Show above="md">
+            <Box h="180px" />
+          </Show>
+          <Heading fontSize="sm" className="fw-lighter">
+            HI. I'M SIMON.
+          </Heading>
+          <Text fontSize="3xl" py={6}>
+            I'm a developer and trainer with a visual approach for your business
+            need and learning target.
+          </Text>
+        </Box>
+      </Center>
+    </SimpleGrid>
+  </Center>
+);
 
 export default Me;
